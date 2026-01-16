c06fed57138dfb1ed0ea8a989b028d2e3c311279
nihal6674 <nmohammaedali@gmail.com>
2026-01-16 17:49:50 +0530

    fix(UI): date advancing on AllEntries

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -2,9 +2,10 @@ export const formatDate = (date) => {
   if (!date) return "";
 
   const d = new Date(date);
-  const mm = String(d.getMonth() + 1).padStart(2, "0");
-  const dd = String(d.getDate()).padStart(2, "0");
-  const yy = String(d.getFullYear()).slice(-2);
+
+  const mm = String(d.getUTCMonth() + 1).padStart(2, "0");
+  const dd = String(d.getUTCDate()).padStart(2, "0");
+  const yy = String(d.getUTCFullYear()).slice(-2);
 
   return `${mm}/${dd}/${yy}`;
 };
